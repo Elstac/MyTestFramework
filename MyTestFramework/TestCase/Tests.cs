38ca8d3b6a1bb881251ee2dbd71557af2141eb22
Jakub Lotko <1elstac1@gmail.com>
2019-06-27 10:54:58 +0200

    Add test for calling test method in test class

---
+++ MyTestFramework/TestCase/Tests.cs
@@ -0,0 +1,19 @@
+﻿using Core;
+using Xunit;
+namespace Tests.TestCase
+{
+    public class Tests
+    {
+        private Core.TestCase testCase;
+
+        [Fact]
+        public void Call_test_method_from_test_class()
+        {
+            testCase = new Core.TestCase(typeof(TestClass));
+
+            testCase.Run("Test");
+
+            Assert.True(testCase.Runned);
+        }
+    }
+}
